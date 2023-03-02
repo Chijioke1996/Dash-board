c10849d67825c8e9556c51ef72e0a0e83581f77b
Chijioke1996 <111643913+Chijioke1996@users.noreply.github.com>
2023-03-02 16:23:12 +0100

    add login and loading symbol2

--- app.js
+++ app.js
@@ -63,6 +63,64 @@ contentContainer.addEventListener("click", () => {
 
 })
 
+// TABLE SOURCE CODE FOR RECENT ACTIVITIES
+async function tableDataUpdate (url, table) {
+    const tableHead = table.querySelector("thead");
+    const tableBody = table.querySelector("tbody");
+    const response = await fetch(url);
+    // const data = await response.json();
+    const {headers, rows} = await response.json();
+    // console.log({headers, rows});
+
+    //TO CLEAR THE TABLE ON REFRESH
+    tableHead.innerHTML = "<tr><tr>"
+    tableBody.innerHTML = " "
+
+    // POPULATING TABLE-HEADING WITH DATA FRON LOCAL JSON FILE
+    for (const headerText of headers) {
+        const headerHtmlElement = document.createElement("th")
+        headerHtmlElement.textContent = headerText
+        tableHead.querySelector("tr").appendChild(headerHtmlElement)
+
+        // console.log(headerText);
+        // console.log(headerHtmlElement);
+        
+        
+        
+    }
+
+    for (const row of rows) {
+       const rowHtmlElement = document.createElement("tr")
+
+       for (const cellText of row) {
+        const cellElement = document.createElement("td")
+        cellElement.textContent = cellText
+        rowHtmlElement.appendChild(cellElement)
+         
+       }
+
+       tableBody.appendChild(rowHtmlElement)
+    }
+
+
+
+
+}
+
+tableDataUpdate("./data.json", document.querySelector(".table"))
+
+// LOADIND PAGE
+
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+  
+    loader.classList.add("loader--hidden");
+  
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+  });
+
 
 
 
